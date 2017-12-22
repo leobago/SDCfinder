2ea259cb19a24769553a5ebe39af3fd5288bc50d
Pau Farré <pau.farre@bsc.es>
2017-12-22 17:14:27 +0100

    Report GPU errors to the logfile instead of stdout.
    Only the first error detected is reported. Subsequent errors are masked.

--- C/lib/tools.cu
+++ C/lib/tools.cu
@@ -1,7 +1,32 @@
 #include <cstdint>
 #include <cstdio>
+#include <cuda_runtime.h>
 
 #include "MemoryReliability_decl.cuh"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "logging.h"
+#ifdef __cplusplus
+};
+#endif
+
+__device__ __managed__ unsigned int fault_masking = 0;
+__device__ __managed__ uintptr_t* faulting_addr;
+__device__ __managed__ uintptr_t faulting_value;
+
+__device__
+bool is_first_error()
+{
+   return !atomicCAS(&fault_masking, 0, 1);
+}
+
+__host__
+bool report_error()
+{
+   return (fault_masking == 1) ? true : false;
+}
 
 __global__
 void check_gpu_kernel(uintptr_t* buffer,
@@ -16,8 +41,12 @@ void check_gpu_kernel(uintptr_t* buffer,
         uintptr_t actual_value = buffer[word];
         if (actual_value != expected_value)
         {
-            printf("ERROR, %p, %p, %p\n", &buffer[word], actual_value, expected_value);
-            //log_error(&buffer[word], actual_value, expected_value);
+            // Check if errors are masked:
+            if (is_first_error())
+            {
+                faulting_addr = &buffer[word];
+                faulting_value = actual_value;
+            }
         }
 
         buffer[word] = new_value;
@@ -29,9 +58,23 @@ void check_gpu_stub(uintptr_t* buffer,
                     uintptr_t expected_value,
                     uintptr_t new_value)
 {
+    // We only report the first error, the rest are ignored
+    fault_masking = 0;
+
     const long unsigned num_words = num_bytes / sizeof(uintptr_t);
     const int block_size = 128;
     const int grid_size = (num_words + block_size - 1) / block_size;
     check_gpu_kernel<<<grid_size, block_size>>>
             (buffer, num_words, expected_value, new_value);
+    cudaError_t err = cudaDeviceSynchronize();
+    if (err != cudaSuccess)
+    {
+        char msg[255];
+        sprintf(msg, "ERROR_INFO, cudaDeviceSynchronize reported an error: (%s:%s).",
+                      cudaGetErrorName(err), cudaGetErrorString(err));
+        log_message(msg);
+    }
+
+    if (report_error())
+        log_error(faulting_addr, faulting_value, expected_value);
 }
