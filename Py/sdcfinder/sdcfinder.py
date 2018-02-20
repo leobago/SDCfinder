791559447d348a1610a233ec327c3dfa7781c4c9
Aleksandr Drozd <blackbird@kiev0.m.gsic.titech.ac.jp>
2018-02-21 00:29:27 +0900

    save metadata to logs

--- Py/sdcfinder/sdcfinder.py
+++ Py/sdcfinder/sdcfinder.py
@@ -1,12 +1,30 @@
 import numpy as np
+import os
 import cupy
 import time
+import json
+import datetime
 from .sysinfo import get_sys_info
 
 
 interval_sleep = 3
-path_logs = "/work/alex/data/memcheck"  # TODO: make this a parameter
+path_root_logs = "/work/alex/data/memcheck"  # TODO: make this a parameter
 metadata = {}
+
+
+def get_time_str():
+    d = datetime.datetime.now()
+    s = d.strftime("%y.%m.%d_%H.%M.%S")
+    return s
+
+
+def save_data_json(data, name_file):
+    path = os.path.realpath(os.path.dirname(name_file))
+    os.makedirs(path, exist_ok=True)
+    s = json.dumps(data, ensure_ascii=False, indent=4, sort_keys=True)
+    f = open(name_file, 'w')
+    print(s, file=f)
+    f.close()
 
 
 def analize(x):
@@ -24,8 +42,10 @@ def get_gpu_mem_size(id_device=0):
 
 
 def run():
+    path_out = os.path.join(path_root_logs, get_time_str())
     metadata["platform"] = get_sys_info()
     print(metadata)
+    save_data_json(metadata, os.path.join(path_out, "metadata.json"))
     size = get_gpu_mem_size()
     x = cupy.zeros(size, dtype=cupy.uint8)
     while True:
